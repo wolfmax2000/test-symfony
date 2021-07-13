d96afeda624546150c7baab6a0834369ac279096
Polosin Maxim <polosinms@mail.ru>
2021-07-13 11:25:03 +0300

    first commit

---
+++ src/Controller/BlogPostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BlogPost;
+use App\Form\BlogPostType;
+use App\Repository\BlogPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog/post")
+ */
+class BlogPostController extends AbstractController
+{
+    /**
+     * @Route("/", name="blog_post_index", methods={"GET"})
+     */
+    public function index(BlogPostRepository $blogPostRepository): Response
+    {
+        return $this->render('blog_post/index.html.twig', [
+            'blog_posts' => $blogPostRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="blog_post_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $blogPost = new BlogPost();
+        $form = $this->createForm(BlogPostType::class, $blogPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($blogPost);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('blog_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('blog_post/new.html.twig', [
+            'blog_post' => $blogPost,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="blog_post_delete", methods={"POST"})
+     */
+    public function delete(Request $request, BlogPost $blogPost): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$blogPost->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($blogPost);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('blog_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
